ff7e5c0ffa4aa712be9fd77ea87ff1f3ea9243bd
Ezequiel Abreu <goetia.dev@proton.me>
2025-09-16 18:46:48 -0300

    vite-config error

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
